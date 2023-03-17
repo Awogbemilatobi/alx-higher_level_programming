168239416215d67330118afe07329a513c755c1d
Awogbemilatobi <105847501+Awogbemilatobi@users.noreply.github.com>
2023-03-17 06:08:43 -0700

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 # Author - Awogbemila Tobi
+"""a program that prints all possible different combinations of two digits. Numbers must be separated by ,, followed by a space
+The two digits must be different. 01 and 10 are considered the same combination of the two digits 0 and 1.Print only the smallest combination of two digits
+Numbers should be printed in ascending order, with two digits
+The last number should be followed by a new line
+You can only use no more than 3 print functions with string format
+You can only use no more than 2 loops in your code
+You are not allowed to store numbers or strings in a variable
+You are not allowed to import any module"""
 
-for digit1 in range(0, 10):
-    for digit2 in range(digit1 + 1, 10):
-        if digit1 == 8 and digit2 == 9:
-            print("{}{}".format(digit1, digit2))
+for i in range(0, 10):
+    for j in range(i+1, 10):
+        if i == j:
+            continue
+        if i == 8 and j == 9:
+            print(f"{i}{j}")
         else:
-            print("{}{}".format(digit1, digit2), end=", ")
+            print(f"{i}{j}, ", end='')

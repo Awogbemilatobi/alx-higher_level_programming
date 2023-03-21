4bdd4affcbf2b19445a94a2044cb931c2fe33824
Awogbemilatobi <105847501+Awogbemilatobi@users.noreply.github.com>
2023-03-20 19:46:14 -0700

    Update 4-only_diff_elements.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+"""Author: Awogbemila Tobi
+a function that returns elements taht appears only once in the two sets"""
+
 def only_diff_elements(set_1, set_2):
-    return (set_1 ^ set_2)
+    set_3 = set_1.symmetric_difference(set_2)
+    return set_3
+

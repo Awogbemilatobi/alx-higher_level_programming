72e55686029bab5a04cdeb5da6f63b4d52f417ed
Awogbemilatobi <105847501+Awogbemilatobi@users.noreply.github.com>
2023-03-20 19:30:14 -0700

    Update 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
+"""Author: Awogbemila Tobi
+a function that adds all unique integers in a list (only once for each integer)"""
+
 def uniq_add(my_list=[]):
-    uniq_list = set(my_list)
-    num = 0
-
-    for i in uniq_list:
-        num += i
-
-    return (num)
+    new_list = set(my_list)
+    sums = sum(new_list)
+    return sums

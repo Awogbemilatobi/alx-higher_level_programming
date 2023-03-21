af0e4d119040416a8d8ab92d253abe92c4343d3d
Awogbemilatobi <105847501+Awogbemilatobi@users.noreply.github.com>
2023-03-21 09:53:23 -0700

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,40 +1,37 @@
-#!/usr/bin/python3
-def to_subtract(list_num):
-    to_sub = 0
-    max_list = max(list_num)
-
-    for n in list_num:
-        if max_list > n:
-            to_sub += n
-
-    return (max_list - to_sub)
-
-
-def roman_to_int(roman_string):
-    if not roman_string:
-        return 0
-
-    if not isinstance(roman_string, str):
-        return 0
-
-    rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    list_keys = list(rom_n.keys())
-
-    num = 0
-    last_rom = 0
-    list_num = [0]
-
-    for ch in roman_string:
-        for r_num in list_keys:
-            if r_num == ch:
-                if rom_n.get(ch) <= last_rom:
-                    num += to_subtract(list_num)
-                    list_num = [rom_n.get(ch)]
-                else:
-                    list_num.append(rom_n.get(ch))
-
-                last_rom = rom_n.get(ch)
-
-    num += to_subtract(list_num)
-
-    return (num)
+def value(r):
+    if (r == 'I'):
+        return 1
+    if (r == 'V'):
+        return 5
+    if (r == 'X'):
+        return 10
+    if (r == 'L'):
+        return 50
+    if (r == 'C'):
+        return 100
+    if (r == 'D'):
+        return 500
+    if (r == 'M'):
+        return 1000
+    return -1
+ 
+def roman_to_int(str):
+    res = 0
+    i = 0
+ 
+    for i in range(len(str)):
+        s1 = value(str[i])   # Getting value of symbol s[i]
+        if (i + 1 < len(str)):            
+            s2 = value(str[i + 1]) # Getting value of symbol s[i + 1]
+            
+            if (s1 >= s2):
+                res = res + s1
+                i = i + 1
+            else:
+                res = res + s2 - s1
+                i = i + 2
+        else:
+            res = res + s1
+            i = i + 1
+    return res
+ 

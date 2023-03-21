5d6f000a1db877d23b922dcc7ba3665b1d98f852
Awogbemilatobi <105847501+Awogbemilatobi@users.noreply.github.com>
2023-03-20 20:32:08 -0700

    Update 7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Author: Awogbemila Tobi
+ a function that replaces or adds key/value in a dictionary."""
+
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
+    a_dictionary.update({key:value})    
     return (a_dictionary)

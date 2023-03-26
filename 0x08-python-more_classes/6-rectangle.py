96d5326c709523644fa758b99715d8f6d42ee7f4
Awogbemilatobi <105847501+Awogbemilatobi@users.noreply.github.com>
2023-03-26 06:10:35 -0700

    Update 6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+#Author: Awogbemila Tobi
 """Defines a Rectangle class."""
 
 
@@ -16,7 +17,7 @@ class Rectangle:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
         """
-        type(self).number_of_instances += 1
+        Rectangle.number_of_instances += 1
         self.width = width
         self.height = height
 
@@ -53,7 +54,7 @@ class Rectangle:
     def perimeter(self):
         """Return the perimeter of the Rectangle."""
         if self.__width == 0 or self.__height == 0:
-            return (0)
+            return ("")
         return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self):
@@ -63,20 +64,24 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ("")
 
-        rect = []
-        for i in range(self.__height):
-            [rect.append('#') for j in range(self.__width)]
-            if i != self.__height - 1:
-                rect.append("\n")
-        return ("".join(rect))
+        else:
+            rect = ""
+            for height in range(self.__height):
+                for width in range(self.__width):
+                    rect += "#"
+                rect += "\n"
+        return (rect)
 
     def __repr__(self):
-        """Return the string representation of the Rectangle."""
-        rect = "Rectangle(" + str(self.__width)
-        rect += ", " + str(self.__height) + ")"
+        """return a string representation of the rectangle
+            to be able to recreate a new instance by using eval()
+        """
+        rect = "Rectangle(" + str(self.__width) + ", " + str(self.__height) + ")" 
+
         return (rect)
+
 
     def __del__(self):
         """Print a message for every deletion of a Rectangle."""
-        type(self).number_of_instances -= 1
+        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
